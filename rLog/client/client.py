dfb4ef21e916b15aeb8602ffc1b49a1df1ce8c71
leon-ha <lekileo@gmail.com>
2024-03-03 01:28:44 +0100

    feat: remote info via env vars

--- rLog/client/client.py
+++ rLog/client/client.py
@@ -1,23 +1,21 @@
+import os
 import time
 import socket
+from rLog import logger
 from random import randint
 from rLog.picklers import serialize
-from rLog import logger
 
 
 class Client:
     def __init__(self, client_id: str):
-        super().__init__()
-        self.client_id = client_id
-
-    def run_process(self):
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.connect(("0.0.0.0", 9999))
+            s.connect((os.environ['rLogRemoteIP'], int(os.environ['rLogRemotePORT'])))
             try:
                 while True:
                     s.sendall(
                         serialize(
-                            timestamp=int(time.time()), device_id=str(self.client_id), streams=["csv", "db"],
+                            timestamp=int(time.time()), device_id=str(client_id),
+                            streams=["csv", "psql"],
                             temperature=randint(-4, 37),
                             humidity=randint(63, 87),
                             state=randint(1, 7)
@@ -38,10 +36,13 @@ class Client:
                 s.close()
                 logger.info("Graceful exit")
 
+    def run_process(self):
+        pass
+
     def kill_process(self):
         pass
 
 
 if __name__ == "__main__":
-    cli_id = input("generator id: ")
+    cli_id = input("add client id: ")
     asdf = Client(cli_id)
