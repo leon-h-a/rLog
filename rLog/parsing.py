c6a63d18c78182c36fbe4b9734ea274798bdd253
leon-ha <lekileo@gmail.com>
2024-03-31 01:51:01 +0000

    style: parsing

--- rLog/parsing.py
+++ rLog/parsing.py
@@ -8,12 +8,11 @@ def serialize(
         payload: dict
         ) -> bytes:
 
-    stringed = json.dumps(
+    return bytes(json.dumps(
         dict(
             ts=ts,
             device_id=device_id,
             streams=streams,
             payload=payload
         )
-    )
-    return bytes(stringed, "ascii")
+    ), "ascii")
