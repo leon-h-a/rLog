02051a1459d55e0bc27700d9ededc375671ac179
leon-ha <lekileo@gmail.com>
2024-03-31 01:47:18 +0000

    refactor: q, dispatch, handler, add logger

--- rLog/dispatch.py
+++ rLog/dispatch.py
@@ -1,59 +1,46 @@
 import os
 import socket
+from rLog import logger
+from rLog.handler import Handler
+from multiprocessing import Process
 
 
 class Dispatch:
     def __init__(self):
-        self.cli_conn: socket = None
-        self.que_conn: socket = None
-        self.create_sockets()
+        self.active_handlers = []
 
-    def create_sockets(self):
-        self.cli_conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.que_conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.que_conn.connect(("0.0.0.0", 7777))
-
-    def run_process(self):
+    def handle_clients(self):
         host = os.environ['rLogLocalIP']
         port = int(os.environ['rLogLocalPORT'])
 
-        print("dispatch online")
-        # self.que_conn.send(b"hello")
-        # resp = self.que_conn.recv(100)
-        # print(f"init ping resp: {resp}")
+        logger.info("Dispatch is online")
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.bind((host, port))
+        s.listen()
 
-        with self.cli_conn as s:
-            s.bind((host, port))
-            s.listen()
-            try:
-                conn, addr = s.accept()
-                while True:
+        while True:
+            # todo: tls, auth, ddos
+            conn, addr = s.accept()
+            handler = Handler(conn=conn)
+            proc = Process(target=handler.handle_client)
+            proc.start()
+            self.active_handlers.append(proc)
 
-                    data = conn.recv(1024)
-                    if not data:
-                        print("client closed connection")
-                        break
-                    else:
-                        print(f"got data: {data}")
-
-                        q_resp = self.que_conn.send(data)
-                        print(f"q resp: {q_resp}")
-
-                        conn.sendall(bytes("ACK", "ascii"))
-
-            except Exception as err:
-                raise err
-
-            finally:
-                self.kill_process()
-
-    def kill_process(self):
-        self.cli_conn.close()
-        self.que_conn.close()
-        print("gracefull exit")
+    def shutdown(self):
+        for proc in self.active_handlers:
+            proc.terminate()
 
 
 if __name__ == "__main__":
     dispatch = Dispatch()
-    dispatch.run_process()
+    try:
+        dispatch.handle_clients()
 
+    except Exception as err:
+        raise err
+
+    except KeyboardInterrupt:
+        pass
+
+    finally:
+        dispatch.shutdown()
