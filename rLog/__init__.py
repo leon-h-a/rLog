b7b345183077d8cffb00112f8cc488bb7ed392d2
leon-ha <lekileo@gmail.com>
2024-05-04 10:41:52 +0200

    style(init): logger

--- rLog/__init__.py
+++ rLog/__init__.py
@@ -3,8 +3,9 @@ import logging
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 ft = logging.Formatter(
-        '%(asctime)s %(name)s %(levelname)s: %(message)s'
-        )
+    "%(asctime)s -> %(levelname)s"
+    "\t[%(filename)s][L%(lineno)d]: %(message)s"
+)
 ch = logging.StreamHandler()
 ch.setFormatter(ft)
 logger.addHandler(ch)
