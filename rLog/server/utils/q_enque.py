6c1a0eccef2930a07d60a8dd9f060934f80eeb9c
leon-ha <lekileo@gmail.com>
2024-07-02 00:03:58 +0200

    feat(enque): logic for opening queue connections
    Add support for connecting single enque to multiple local
    queue instances.

--- rLog/server/utils/q_enque.py
+++ rLog/server/utils/q_enque.py
@@ -1,48 +1,81 @@
+import json
 import socket
-import inspect
-from abc import ABCMeta
-import rLog.server.streams as sms
-from rLog.server.streams import Stream
 from rLog.server import logger
-
-classes = inspect.getmembers(sms, inspect.isclass)
-streams = [
-    cls[1] for cls in classes if not issubclass(cls[1], ABCMeta) and
-    cls[1] is not Stream
-]
+from rLog.server.streams import Stream
+from rLog.responses import Valid, Error
 
 
 class Enqueuer:
     def __init__(self, conn: socket):
         self.cli_conn = conn
-        self.que_conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        # todo: read message and then connect to queues if not already conn
-        # self.que_conn.connect(("0.0.0.0", 7777))
-        logger.info(f"[{self.cli_conn}] new connection")
+        self.peer = conn.getpeername()[0]
+
+        self.streams = dict()
+        for stream in Stream.__subclasses__():
+            self.streams[stream.name] = dict(
+                stream_def=stream,
+                q_sock=None
+            )
 
     def handle_client(self):
-        logger.info(f"[{self.cli_conn}] running")
+        logger.info(f"[{self.peer}] handler running")
         while True:
             data = self.cli_conn.recv(1024)
-
-            # todo: check if queue connection, if not create
-            #       keep timeout and close if not used
-
             if not data:
-                logger.info(f"[{self.cli_conn}] closed connection")
-                self.que_conn.send(b"")  # Trigger queue handler close conn
+                logger.info(f"[{self.peer}] client diconnected")
+                # todo: break queue connections before exiting
                 break
 
-            else:
-                logger.debug(f"client sent: {data}")
+            payload = json.loads(data)
+            print(payload)
 
-                # todo: parse data by using streams.py
+            # +------------------------------+
+            # |   Queue handling             |
+            # +------------------------------+
+            try:
+                if not payload["outputs"]:
+                    self.cli_conn.send(Error("Outputs field is empty"))
 
-                self.que_conn.send(data)
-                q_resp = self.que_conn.recv(1024)
-                logger.debug(f"queue resp: {q_resp}")
+            except KeyError:
+                self.cli_conn.send(Error("No outputs field"))
 
-                self.cli_conn.sendall(q_resp)
+            outputs = payload["outputs"]
+            for output in outputs:
+                if output not in self.streams.keys():
+                    self.cli_conn.send(Error("Output not supported"))
+                    continue
+                else:
+                    stream = self.streams[output]
+
+                q_sock = stream["q_sock"]
+                if q_sock is None:
+                    try:
+                        q_sock = socket.socket(
+                            socket.AF_INET, socket.SOCK_STREAM
+                        )
+                        q_sock.connect((
+                            "localhost", stream["stream_def"].port
+                        ))
+                        self.streams[output]["q_sock"] = q_sock
+
+                    except ConnectionRefusedError:
+                        self.cli_conn.send(Error("Queue offline"))
+
+                    except Exception as e:
+                        raise e
+
+            self.cli_conn.send(Valid("Good msg"))
+
+            # +------------------------------+
+            # |   Message sanitization       |
+            # +------------------------------+
+            # todo: parse data by using streams.py
+            pass
+
+            # +------------------------------+
+            # |   Enque                      |
+            # +------------------------------+
+            pass
 
 
 if __name__ == "__main__":
