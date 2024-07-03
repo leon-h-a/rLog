13614f2d360d0008f39c0b35f2dc22cf55e3fb1c
leon-ha <lekileo@gmail.com>
2024-07-04 00:36:35 +0200

    refactor(streams)

--- rLog/server/streams.py
+++ rLog/server/streams.py
@@ -1,4 +1,3 @@
-import json
 from abc import ABCMeta
 from rLog.responses import Error, Valid
 from rLog.server import logger
@@ -7,39 +6,26 @@ from rLog.server import logger
 class Stream(metaclass=ABCMeta):
     name: str
     port: None
+    required_fields = ["ts", "payload", "client_id"]
 
-    def __init__(self):
-        self.resp = {
-            "timestamp": "Field 'timestamp' is not present",
-            "streams": "Value 'csv' not present in streams field",
-            "payload": "Field 'payload' is not present"
-        }
-
-    def _sanitize_required(self, msg: str):
-        logger.debug(msg)
-        try:
-            json.loads(str(msg).replace("'", '"'))
-            for key in self.resp.keys():
-                msg[key]
-
-        except KeyError as e:
-            return Error(text=self.resp[e.args[0]])
-
-        except Exception as e:
-            raise e
-            return Error(text=str(e))
-
-        return Valid(text="Message passed to queue")
-
-    def sanitize(self, msg: str):
+    def output_setup():
         """
-        check data validity and respond with ACK/NACK
+        Execute output setup (ie. dir setup, db connection...).
         """
         raise NotImplementedError
 
-    def generate(self):
+    def input_sanitize(payload: dict) -> [Valid, Error]:
         """
-        generate object ready for storage
+        Defines what fields for specific streams are required.
+        Used by q_enque.py to sanitize and push to queue.
+        """
+        raise NotImplementedError
+
+    def output_generate():
+        """
+        Defines how payload is written to output.
+        Used by q_deque.py for writing data to output that was set up in
+        output_setup def.
         """
         raise NotImplementedError
 
@@ -47,34 +33,42 @@ class Stream(metaclass=ABCMeta):
 class CSV(Stream):
     name = "csv"
     port = 8810
+    required_field = Stream.required_fields + ["filepath"]
 
-    def __init__(self):
-        super().__init__()
-        # add additional fields that are needed in dequeuer.py
-        self.resp["filepath"] = "Field 'filepath' is not present"
+    def output_setup():
+        # make sure directory exists (?)
+        pass
 
-    def sanitize(self, msg: str):
-        basic_check = self._sanitize_required(msg=msg)
-        # Add specific checks
-        return basic_check
+    def input_sanitize(payload: dict):
+        for req in CSV.required_fields:
+            if req not in payload.keys():
+                return Error(f"field [{req}] not present in payload", False)
 
-    def generate(self):
+        # todo: sanitize values (ts: int, client_id: str ...)
+        pass
+
+        # todo: return response and payload that excludes values needed for
+        #       other Streams to reduce memory required for queues
+        # note: Stream specific fields are negligible in size compared to
+        #       payload size. Stripping might not be needed after all.
+        pass
+
+    def output_generate():
+        # todo: implement log rotation based on client id
         pass
 
 
 class Influx(Stream):
     name = "influx"
     port = 8811
+    required_fields = Stream.required_fields + ["bucket"]
 
-    def __init__(self):
-        super().__init__()
-        # add additional fields that are needed in dequeuer.py
-        self.resp["bucket"] = "Field 'bucket' is not present"
+    def output_setup():
+        # influx db connection
+        pass
 
-    def sanitize(self, msg: str):
-        basic_check = self._sanitize_required(msg=msg)
-        # Add specific checks
-        return basic_check
+    def input_sanitize(payload: dict):
+        pass
 
-    def generate(self):
+    def output_generate():
         pass
